5c073a57a76f3942ff214e316ef865d1e74e7d61
Dennis Tse <dennis.tse@lalamove.com>
2018-07-18 09:30:49 +0800

    UPT: export Select

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ export { default as BaseApp } from './components/BaseApp';
 export { default as Checkbox } from './components/Checkbox';
 export { default as Radio, RadioGroup } from './components/Radio';
 export { default as Input } from './components/Input';
+export { default as AnimatedInput } from './components/Input';
+export { default as Select } from './components/Select';
 
 // for icons, export them here
 export { default as ErrorIcon } from './icons/ErrorIcon';
