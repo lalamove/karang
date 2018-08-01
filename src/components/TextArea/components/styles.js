8a674dee58822b18ce790b055b959158937a61b5
Don Dyu <ddkim@connect.ust.hk>
2018-08-01 15:57:23 +0800

    Refactored code, added Excess character message and Characters left message

---
+++ src/components/TextArea/components/styles.js
@@ -0,0 +1,73 @@
+import styled from 'styled-components';
+
+import { orange, lightGray, red } from 'styles/colors';
+import { primaryFonts, fontSize } from 'styles/fonts';
+
+const TextAreaContainer = styled.div`
+  height: calc(100% - 24px);
+  width: calc(100% - 24px);
+  padding: 11px;
+  border: 1px solid ${lightGray};
+  ${({ focused }) =>
+    focused &&
+    `
+    border: 1px solid ${orange};
+    `};
+  ${({ error }) =>
+    error &&
+    `
+    border: 1px solid ${red};
+    `};
+`;
+
+const CharacterLimitDisplay = styled.div`
+  font-size: ${fontSize.small};
+  font-family: ${primaryFonts};
+  line-height: 16px;
+  color: lightGray;
+`;
+
+const InnerInputArea = styled.textarea`
+  font-family: ${primaryFonts};
+  width: 100%;
+  height: calc(100% - 11px);
+  border: 0px solid transparent;
+  padding: 0px;
+  resize: none;
+  &:focus {
+    outline-width: 0;
+  }
+
+  ${({ hasCharacterLimit }) =>
+    hasCharacterLimit ? `height: calc(100% - 11px);` : `height: 100%;`};
+
+  &::-webkit-input-placeholder {
+    /* Chrome/Opera/Safari */
+    color: lightGray;
+    font-size: ${fontSize.regular};
+    font-family: ${primaryFonts};
+  }
+
+  &::-moz-placeholder {
+    /* Firefox 19+ */
+    color: lightGray;
+    font-size: ${fontSize.regular};
+    font-family: ${primaryFonts};
+  }
+
+  &:-ms-input-placeholder {
+    /* IE 10+ */
+    color: lightGray;
+    font-size: ${fontSize.regular};
+    font-family: ${primaryFonts};
+  }
+
+  &:-moz-placeholder {
+    /* Firefox 18- */
+    color: lightGray;
+    font-size: ${fontSize.regular};
+    font-family: ${primaryFonts};
+  }
+`;
+
+export { TextAreaContainer, CharacterLimitDisplay, InnerInputArea };
--- src/components/TextArea/index.js
+++ src/components/TextArea/index.js
@@ -1,3 +1,137 @@
-import TextArea from './components/TextArea';
+import React, { Component } from 'react';
+import { number, string, func, shape, bool } from 'prop-types';
 
-export { TextArea as default };
+import noop from 'utils/noop';
+import {
+  TextAreaContainer,
+  InnerInputArea,
+  CharacterLimitDisplay,
+} from './components/styles';
+import withErrorMessage from 'hoc/withErrorMessage';
+
+// Presentational Component (Layout)
+const PresentationalTextArea = props => {
+  const {
+    characterLimit,
+    displayedCharacterLimitMsg,
+    charactersLeft,
+    style,
+    error,
+    focused,
+    ...remainProps
+  } = props;
+  return (
+    <TextAreaContainer style={style} error={error} focused={focused}>
+      <InnerInputArea hasCharacterLimit={!!characterLimit} {...remainProps} />
+      <CharacterLimitDisplay>
+        {displayedCharacterLimitMsg}
+      </CharacterLimitDisplay>
+    </TextAreaContainer>
+  );
+};
+
+PresentationalTextArea.propTypes = {
+  characterLimit: number,
+  displayedCharacterLimitMsg: string,
+  charactersLeft: number,
+  error: string,
+  focused: bool,
+  style: shape({}),
+};
+
+PresentationalTextArea.defaultProps = {
+  characterLimit: null,
+  displayedCharacterLimitMsg: null,
+  charactersLeft: null,
+  style: {},
+  error: null,
+  focused: false,
+};
+
+// HOC
+const PresentationalTextAreaWithError = withErrorMessage(
+  PresentationalTextArea
+);
+
+// Container Component (Logic)
+class TextArea extends Component {
+  static propTypes = {
+    characterLimit: number,
+    onChange: func,
+    style: shape({}),
+    errorMsg: string,
+    characterLimitMsgGenerator: func,
+    exceedLimitMsgGenerator: func,
+  };
+  static defaultProps = {
+    characterLimit: null,
+    onChange: noop,
+    style: {},
+    errorMsg: 'Character limit exceeded',
+    characterLimitMsgGenerator: charactersLeft =>
+      `Characters left: ${charactersLeft}`,
+    exceedLimitMsgGenerator: excessCharacters =>
+      `Excess characters: ${excessCharacters}`,
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      charactersLeft: props.characterLimit,
+      focused: false,
+    };
+  }
+  onTextAreaChange = e => {
+    if (this.props.characterLimit) {
+      const charactersLeft = this.props.characterLimit - e.target.value.length;
+      this.setState({
+        charactersLeft,
+      });
+    }
+    this.props.onChange(e);
+  };
+  onFocus = e => {
+    this.setState({
+      focused: true,
+    });
+  };
+  onBlur = e => {
+    this.setState({
+      focused: false,
+    });
+  };
+  render() {
+    const { charactersLeft, focused } = this.state;
+    const {
+      characterLimit,
+      errorMsg,
+      onChange: defaultOnChange,
+      characterLimitMsgGenerator: defaultCharacterLimitMsgGenerator,
+      exceedLimitMsgGenerator: defaultExceedLimitMsgGenerator,
+      ...remainProps
+    } = this.props;
+    let displayedCharacterLimitMsg;
+    if (characterLimit) {
+      displayedCharacterLimitMsg =
+        charactersLeft >= 0
+          ? defaultCharacterLimitMsgGenerator(charactersLeft)
+          : defaultExceedLimitMsgGenerator(-charactersLeft);
+    } else {
+      displayedCharacterLimitMsg = null;
+    }
+    return (
+      <PresentationalTextAreaWithError
+        characterLimit={characterLimit}
+        charactersLeft={charactersLeft}
+        onChange={this.onTextAreaChange}
+        focused={focused}
+        onFocus={this.onFocus}
+        onBlur={this.onBlur}
+        error={charactersLeft < 0 ? errorMsg : null}
+        displayedCharacterLimitMsg={displayedCharacterLimitMsg}
+        {...remainProps}
+      />
+    );
+  }
+}
+
+export default TextArea;
