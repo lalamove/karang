3c9161ba8f854bbc30699eaab0d2221dbb6efafe
Andrew Mok <andrew.mok@lalamove.com>
2018-08-10 12:16:05 +0800

    FIX: Blank page in driver app webview

--- src/styles/fonts.js
+++ src/styles/fonts.js
@@ -2,12 +2,9 @@ import { injectGlobal } from 'styled-components';
 
 injectGlobal`
   @import url(https://fonts.googleapis.com/css?family=Noto+Sans:400,700);
-  @import url(https://fonts.googleapis.com/earlyaccess/notosansjapanese.css);
-  @import url(https://fonts.googleapis.com/earlyaccess/notosanstc.css);
 `;
 
-export const primaryFonts =
-  'Noto Sans, "Noto Sans TC", "Noto Sans JP", sans-serif';
+export const primaryFonts = '"Noto Sans", sans-serif';
 
 export const fontSize = {
   micro: '10px',
