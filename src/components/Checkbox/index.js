9b9cd6821710cb3477da3b58f6d88a953d6db038
Jake Choi <jake.choi@lalamove.com>
2018-08-22 18:10:36 +0800

    REF: reorg code

--- src/components/Checkbox/index.js
+++ src/components/Checkbox/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { string, bool, func, node } from 'prop-types';
 import styled from 'styled-components';
 
@@ -6,120 +6,126 @@ import noop from 'utils/noop';
 import { orange, gray, silver, white, offWhite } from 'styles/colors';
 import { primaryFonts, fontSize } from 'styles/fonts';
 
-const Text = styled.span`
-  padding-left: 28px;
-`;
-
-const Checkmark = styled.span`
-  border: solid 1px ${silver};
-  height: 16px;
-  width: 16px;
-  position: absolute;
-
-  &:after {
-    content: '';
-    display: none;
-    position: absolute;
-    border: solid ${orange};
-    border-width: 0 2px 2px 0;
-    height: 9px;
-    width: 4px;
-    left: 5px;
-    top: 1px;
-    transform: rotate(45deg);
-  }
+const Container = styled.label`
+  position: relative;
+  display: inline-flex;
+  align-items: center;
+  color: ${gray};
+  font-family: ${primaryFonts};
+  cursor: pointer;
+  font-size: ${fontSize.regular};
+  user-select: none;
+  line-height: 20px;
 `;
 
 const Input = styled.input`
+  position: absolute;
+  overflow: hidden;
   width: 1px;
   height: 1px;
   padding: 0;
   border: 0;
   margin: -1px;
   clip: rect(0, 0, 0, 0);
-  overflow: hidden;
-  position: absolute;
+`;
 
-  &:focus ~ ${Checkmark} {
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.22);
-  }
+const Text = styled.span`
+  padding-left: 28px;
 
-  &:checked ~ ${Checkmark} {
-    background-color: ${white};
-  }
-
-  &:checked ~ ${Checkmark}:after {
-    display: block;
-  }
-
-  &:disabled ~ ${Checkmark} {
-    background-color: ${offWhite};
-  }
-
-  &:disabled ~ ${Checkmark}:after {
-    border-color: ${silver};
-  }
-
-  &:disabled ~ ${Text} {
+  ${Input}:disabled ~ & {
     opacity: 0.7;
   }
 `;
 
-const Container = styled.label`
-  position: relative;
-  font-family: ${primaryFonts};
-  color: ${gray};
-  cursor: pointer;
-  display: inline-flex;
-  align-items: center;
-  font-size: ${fontSize.regular};
-  user-select: none;
-  line-height: 20px;
+const Checkmark = styled.span`
+  position: absolute;
+  width: 16px;
+  height: 16px;
+  border: solid 1px ${silver};
 
-  &:hover ${Input} ~ ${Checkmark} {
-    border: solid 1px ${silver};
+  &:after {
+    content: '';
+    position: absolute;
+    top: 1px;
+    left: 5px;
+    display: none;
+    width: 4px;
+    height: 9px;
+    border: solid ${orange};
+    border-width: 0 2px 2px 0;
+    transform: rotate(45deg);
   }
 
-  &:hover ${Checkmark} {
+  ${Input}:focus ~ & {
     box-shadow: 0 2px 4px rgba(0, 0, 0, 0.22);
+  }
+
+  ${Input}:checked ~ & {
+    background-color: ${white};
+  }
+
+  ${Input}:checked ~ &:after {
+    display: block;
+  }
+
+  ${Input}:disabled ~ & {
+    background-color: ${offWhite};
+  }
+
+  ${Input}:disabled ~ &:after {
+    border-color: ${silver};
+  }
+
+  ${Container}:hover & {
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.22);
+  }
+
+  ${Container}:hover ${/* sc-selector */ Input} ~ & {
+    border: solid 1px ${silver};
   }
 `;
 
-const CheckBox = ({
-  checked,
-  name,
-  label,
-  onChange,
-  disabled,
-  ...remainProps
-}) => (
-  <Container htmlFor={name} {...remainProps}>
-    <Input
-      type="checkbox"
-      name={name}
-      id={name}
-      checked={checked}
-      onChange={onChange}
-      disabled={disabled}
-    />
-    <Checkmark />
-    <Text>{label}</Text>
-  </Container>
-);
+class CheckBox extends Component {
+  static defaultProps = {
+    name: 'llm-checkbox',
+    label: '',
+    checked: false,
+    onChange: noop,
+    disabled: false,
+  };
 
-CheckBox.propTypes = {
-  name: string.isRequired, // eslint-disable-line
-  label: node,
-  checked: bool,
-  onChange: func,
-  disabled: bool,
-};
+  static propTypes = {
+    name: string,
+    label: node,
+    checked: bool,
+    onChange: func,
+    disabled: bool,
+  };
 
-CheckBox.defaultProps = {
-  label: '',
-  checked: false,
-  onChange: noop,
-  disabled: false,
-};
+  render() {
+    const {
+      checked,
+      name,
+      label,
+      onChange,
+      disabled,
+      ...remainProps
+    } = this.props;
+    return (
+      <Container htmlFor={name} {...remainProps}>
+        <Input
+          type="checkbox"
+          name={name}
+          id={name}
+          checked={checked}
+          onChange={onChange}
+          disabled={disabled}
+        />
+        <Checkmark />
+        <Text>{label}</Text>
+      </Container>
+    );
+  }
+}
 
 export default CheckBox;
