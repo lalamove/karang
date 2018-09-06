b5af5d42244c10b5c18fdb599ec46a2a2b950404
Karan Grover <karan.grover@lalamove.com>
2018-09-06 17:10:25 +0800

    temporary changes

---
+++ src/components/DatePicker/_story.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import LmDatePicker from './index';
+import moment from 'moment';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: block;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-items: center;
+  width: 400px;
+`;
+
+const DatePickerStory = () => {
+  console.log(
+    moment()
+      .add(3, 'months')
+      .toString()
+  );
+  console.log(
+    moment()
+      .add(3, 'months')
+      .add(10, 'days')
+  );
+  return (
+    <Container>
+      <LmDatePicker initialStartDate={null} initialEndDate={null} />
+    </Container>
+  );
+};
+
+storiesOf('Date Picker', module).add('DatePicker', () => <DatePickerStory />);
