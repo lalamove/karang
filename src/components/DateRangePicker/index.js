9c0ea7a15e966d4fbd14543b4fec63c60c4a1c76
Karan Grover <karan.grover@lalamove.com>
2018-09-14 13:51:25 +0800

    REF: removed derivedStateFromProps lifecycle method

--- src/components/DateRangePicker/index.js
+++ src/components/DateRangePicker/index.js
@@ -15,30 +15,6 @@ const START_DATE = 'startDate';
 const END_DATE = 'endDate';
 
 class DateRangePicker extends Component {
-  static getDerivedStateFromProps(nextProps, prevState) {
-    if (nextProps.onDatesChange !== noop) {
-      if (nextProps.startDate !== prevState.startDate) {
-        if (nextProps.startDate) {
-          return { startDate: nextProps.startDate };
-        }
-      } else if (nextProps.endDate !== prevState.endDate) {
-        if (nextProps.endDate) {
-          return { endDate: nextProps.endDate };
-        }
-      }
-    }
-
-    if (nextProps.onFocusChange !== noop) {
-      if (nextProps.focusedInput !== prevState.focusedInput) {
-        if (nextProps.focusedInput) {
-          return { focusedInput: nextProps.focusedInput };
-        }
-      }
-    }
-
-    return null;
-  }
-
   constructor(props) {
     super();
 
