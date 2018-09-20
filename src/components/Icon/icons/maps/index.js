ba1ff0a83bf6fd59490e18e1308bd3df2fe680b2
Andrew Mok <andrew.mok@lalamove.com>
2018-09-20 17:52:16 +0800

    UPT: FOX-1217 Add pins icons

---
+++ src/components/Icon/icons/maps/index.js
@@ -0,0 +1,11 @@
+import midstop from './midstop';
+import pickup from './pickup';
+import pinFill from './pinFill';
+
+const MapsIcons = {
+  midstop,
+  pickup,
+  pinFill,
+};
+
+export default MapsIcons;
