7c8b6e1f1e9fbf1739aba745d44bcf72cfd2fd3f
Andrew Mok <andrew.mok@lalamove.com>
2018-12-03 18:22:22 +0800

    FIX: FOX-1946 Update styled-component outdated usage (extend)

--- src/components/Button/components/LinkButton.js
+++ src/components/Button/components/LinkButton.js
@@ -1,7 +1,8 @@
+import styled from 'styled-components';
 import { withComponent } from './Button';
 
 const Link = withComponent('a');
-const LinkButton = Link.extend`
+const LinkButton = styled(Link)`
   border-radius: 2px;
   margin-top: 20px;
   text-align: center;
