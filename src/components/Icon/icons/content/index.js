a861bf79c9e5d46a35a0e46d17a609b9d68fd6c6
Jake Choi <jake.choi@lalamove.com>
2018-12-05 15:39:55 +0800

    ADD: language icon

--- src/components/Icon/icons/content/index.js
+++ src/components/Icon/icons/content/index.js
@@ -1,9 +1,11 @@
 import addstop from './addstop';
 import vmenu from './vmenu';
+import language from './language';
 
 const ContentIcons = {
   addstop,
   vmenu,
+  language,
 };
 
 export default ContentIcons;
