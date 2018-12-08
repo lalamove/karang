57e4373de2bb8b4d138e4df891dd727a91ed8324
Jake Choi <jake.choi@lalamove.com>
2018-12-08 21:25:40 +0800

    FIX: input bottom padding for error msg

--- src/components/Input/components/InputComp.js
+++ src/components/Input/components/InputComp.js
@@ -1,11 +1,11 @@
 import React, { Component, forwardRef } from 'react';
 import { bool, func, string, object, oneOfType, shape } from 'prop-types';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import noop from 'utils/noop';
 import compose from 'utils/compose';
 import AnimatedBorder from 'components/AnimatedBorder';
-import ErrorMessage from 'components/ErrorMessage';
+import RawErrorMsg from 'components/ErrorMessage';
 import withSelectAll from 'hoc/withSelectAll';
 import withCursorEnd from 'hoc/withCursorEnd';
 import TextInput from './TextInput';
@@ -18,7 +18,21 @@ const SCTextInput = styled(TextInput)`
 `;
 
 const Wrapper = styled.div`
+  position: relative;
   display: inline-block;
+
+  ${({ error }) =>
+    error &&
+    css`
+      padding-bottom: 2em;
+    `};
+`;
+
+const ErrorMessage = styled(RawErrorMsg)`
+  position: absolute;
+  z-index: 1;
+  right: 0;
+  left: 0;
 `;
 
 const propTypes = {
@@ -145,7 +159,7 @@ class Comp extends Component {
     } = this.props;
 
     return (
-      <Wrapper style={style} className={className}>
+      <Wrapper style={style} className={className} error={error}>
         <AnimatedBorder
           name={name}
           label={label}
@@ -173,19 +187,19 @@ class Comp extends Component {
   }
 }
 
-const Input = forwardRef((props, ref) => (
+const InputComp = forwardRef((props, ref) => (
   <Comp forwardedRef={ref} {...props} />
 ));
 
-const InputComp = compose(
+const Input = compose(
   withSelectAll,
   withCursorEnd
-)(Input);
+)(InputComp);
 
 // Ugly fix for React Styleguidist as it cannot recognize forwardRef
 // const Input = ({ forwardedRef, ...props }) => <InputComp {...props} />;
-// Input.displayName = 'Input';
-// Input.propTypes = propTypes;
-// Input.defaultProps = defaultProps;
+Input.displayName = 'Input';
+Input.propTypes = propTypes;
+Input.defaultProps = defaultProps;
 
-export default InputComp;
+export default Input;
