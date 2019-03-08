a8247f36f0d3d4498d3e47ac6e4737fe64a937da
Andrew Mok <andrew.mok@lalamove.com>
2019-03-08 16:15:02 +0800

    UPT: Update Documentation

--- src/components/Spinner/_story.js
+++ src/components/Spinner/_story.js
@@ -4,14 +4,10 @@ import { storiesOf } from '@storybook/react';
 import Spinner from './index';
 
 storiesOf('Spinner', module)
-  .add('Default', () => (
-    <div>
-      <Spinner /> <span>Hello</span>
-    </div>
-  ))
-  .add('Big', () => <Spinner size="large" />)
-  .add('Color', () => <Spinner color="#d8534f" size="large" />)
-  .add('Dark BG', () => (
+  .add('Default', () => <Spinner />)
+  .add('Large', () => <Spinner size="large" />)
+  .add('Color', () => <Spinner color="#2FA0ED" size="large" />)
+  .add('Dark background', () => (
     <div style={{ background: '#303030', padding: '100px' }}>
       <Spinner color="#ffffff" size="large" />
     </div>
