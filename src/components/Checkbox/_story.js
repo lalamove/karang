785b28eccf12245f0bc5fda7d24cfce63cc7638f
Ignatius <ignatius.baptista@lalamove.com>
2019-03-21 10:44:52 +0800

    UPT: doc, style, props, test

--- src/components/Checkbox/_story.js
+++ src/components/Checkbox/_story.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { boolean } from '@storybook/addon-knobs/react';
+import QuestionIcon from 'components/Icon/icons/alert/question';
+import { primary } from 'styles/colors';
 import Checkbox from './index';
 
-storiesOf('Checkbox', module).add('Basic', () => (
-  <Checkbox label="Checkbox" disabled={boolean('disabled', false)} />
-));
+storiesOf('Checkbox', module)
+  .add('Basic', () => (
+    <Checkbox
+      label="Checkbox"
+      disabled={boolean('disabled', false)}
+      highlightLabel={boolean('highlightLabel', false)}
+      title="A simple checkbox"
+    />
+  ))
+  .add('With icon', () => (
+    <Checkbox
+      label="Checkbox"
+      disabled={boolean('disabled', false)}
+      highlightLabel={boolean('highlightLabel', false)}
+      title="A simple checkbox with icon"
+      icon={<QuestionIcon color={primary.main} />}
+    />
+  ));
