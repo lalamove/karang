b67888e933029f7db9e133b2af6be2c253fa0a07
Gaurav <grvmithas@gmail.com>
2019-11-25 15:02:03 +0530

    toggle updated

--- src/components/Toggle/_story.js
+++ src/components/Toggle/_story.js
@@ -2,7 +2,12 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { boolean } from '@storybook/addon-knobs';
 import Toggle from './index';
+import BaseApp from '../BaseApp';
 
 storiesOf('Toggle', module).add('Basic', () => (
-  <Toggle disabled={boolean('disabled', false)} title="A toggle switch" />
+  <div dir={boolean('Right-to-Left', false) ? 'rtl' : 'ltr'}>
+    <BaseApp rtl={boolean('Right-to-Left', false)}>
+      <Toggle disabled={boolean('disabled', false)} title="A toggle switch" />
+    </BaseApp>
+  </div>
 ));
